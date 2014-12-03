9b4120313180473fa7cd126e13675b613bdcfadf
Owner <Owner@192.168.2.100>
2014-12-03 23:43:00 +0900

    second commit.

--- src/github/Github.java
+++ src/github/Github.java
@@ -2,6 +2,6 @@ package github;
 
 public class Github {
 	public static void main(String args[]) {
-		System.out.println("Github‚ÆEclipse‚Ì˜AŒg");
+		System.out.println("Githubã¨Eclipseã®é€£æº");
 	}
 }
