50060641fe048607bbb0725d173a31ffef553ed4
Owner <Owner@192.168.2.100>
2014-12-07 20:35:06 +0900

    iii

--- src/github/Github.java
+++ src/github/Github.java
@@ -4,6 +4,7 @@ public class Github {
 	public static void main(String args[]) {
 		System.out.println("GithubとEclipseの連携");
 		System.out.println("不要な出力");
-		System.out.println("必要な出力");
+		System.out.println("必要な出力A");
+		System.out.println("必要な出力B");
 	}
 }
