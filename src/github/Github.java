95cee62cd8e1e1b8adaff89696cffa3c956299f7
Owner <Owner@192.168.2.100>
2014-12-07 20:31:54 +0900

    commit only ｌｏｃａｌ

--- src/github/Github.java
+++ src/github/Github.java
@@ -3,5 +3,7 @@ package github;
 public class Github {
 	public static void main(String args[]) {
 		System.out.println("GithubとEclipseの連携");
+		System.out.println("不要な出力");
+		System.out.println("必要な出力");
 	}
 }
